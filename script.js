3bcfcbd8af5057bb26458ce21b4c673779f65c04
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-09 16:34:55 +0200

    timer set

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ console.log("JS OK");
 // Getting the elements from the DOM
 const timerContainer = document.getElementById("timer-container");
 const numbersContainer = document.getElementById("numbers-container");
+
+// Timer
+let seconds = 30;
+timerContainer.innerText = seconds;
+
+setTimeout(() => {
+  timerContainer.innerText = --seconds;
+  if (seconds === 0) {
+    timerContainer.innerText = "Remember the numbers? Write 'em down!";
+    clearInterval(seconds);
+  }
+}, 1000);
